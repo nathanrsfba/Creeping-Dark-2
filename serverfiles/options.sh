e656c4c21ceec389b8cde839e893cccb4e3e5e81
Nathan Roberts <nathanr@tardislabs.com>
2025-11-18 13:42:34 -0800

    Added an options file for server pack

---
+++ serverfiles/options.sh
@@ -0,0 +1,20 @@
+# Options for mkserverpack.sh.
+# This file will be excluded from the final pack file
+
+# Pack name
+PACKNAME="Creeping-Dark-II"
+# Pack version
+PACKVER="2.0.1"
+# Server pack version
+SERVERVER="1"
+# Name of directory within zip to place files
+SERVERDIR="cd2server"
+
+# Folders to package
+FOLDERS="config mods defaultconfigs openloader scripts worldshape"
+# Mods to exclude
+EXCLUDE="Ding MouseTweaks ReAuth ToastControl"
+# Path to mods
+MODDIR="./mods"
+# Staging directory
+TMP="./tmp"
